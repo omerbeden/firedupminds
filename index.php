ce431193dd9fec2affce7209551005f0eb7fdac8
omerbeden <omerbeden@gmail.com>
2024-01-15 21:12:52 +0300

    update random limit

--- index.php
+++ index.php
@@ -72,7 +72,7 @@
 
   <script src="redirect.js"></script>
   <script>
-    var randomNumber = generateRandomNumber(18);
+    var randomNumber = generateRandomNumber(27);
     var newImageURL = `images/${randomNumber}.jpg`;
 
     var dynamicImageElement = document.getElementById("dynamicTwitterImage");
